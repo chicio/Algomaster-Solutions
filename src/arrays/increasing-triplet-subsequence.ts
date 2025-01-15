6cd2b9579cee0aea8cbf9496e93241c31cf2a980
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-01-15 22:16:14 +0100

    Adding exercises :rocket:

---
+++ src/arrays/increasing-triplet-subsequence.ts
@@ -0,0 +1,55 @@
+/**
+ * https://leetcode.com/problems/increasing-triplet-subsequence/description/
+ * 334. Increasing Triplet Subsequence
+ *
+ *  Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,4,5]
+ * Output: true
+ * Explanation: Any triplet where i < j < k is valid.
+ * Example 2:
+ *
+ * Input: nums = [5,4,3,2,1]
+ * Output: false
+ * Explanation: No triplet exists.
+ * Example 3:
+ *
+ * Input: nums = [2,1,5,0,4,6]
+ * Output: true
+ * Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 5 * 105
+ * -231 <= nums[i] <= 231 - 1
+ *
+ *
+ * Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+ */
+
+function increasingTriplet(nums: number[]): boolean {
+    let i = Infinity
+    let j = Infinity
+
+    for (let current = 0; current < nums.length; current++) {
+        let currentI = nums[i] ?? Infinity
+        let currentK = nums[j] ?? Infinity
+
+        if (nums[current] <= currentI) {
+            i = current
+        } else if (nums[current] <= currentK) {
+            j = current
+        } else {
+            return true
+        }
+    }
+
+    return false
+}
+
+console.log([2,1,5,0,4,6])
