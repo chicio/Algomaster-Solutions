49b088e8c894a3b830ea8ee367d04e5b1e94f3be
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-01-25 14:25:34 +0100

    New exercises :rocket:

--- src/bit-manipulation/number-of-1-bits.ts
+++ src/bit-manipulation/number-of-1-bits.ts
@@ -1,10 +1,8 @@
 /**
+ * https://leetcode.com/problems/number-of-1-bits/
  * 191. Number of 1 Bits
- * Solved
- * Easy
  *
- * Topics
- * Companies
+ * 
  * Given a positive integer n, write a function that returns the number of 
  * set bits
  * in its binary representation (also known as the Hamming weight).
