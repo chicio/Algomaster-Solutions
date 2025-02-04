68c12b80dbac7ac68c327c821e752ee2ebb3c302
chicio <fabrizio.duroni@gmail.com>
2025-02-04 23:08:02 +0100

    Adding exercises :rocket:

---
+++ src/hash-tables/ group-anagrams.ts
@@ -0,0 +1,58 @@
+/**
+ * 49. Group Anagrams
+ * 
+ * Given an array of strings strs, group the 
+ * anagrams
+ *  together. You can return the answer in any order.
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * 
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 
+ * Explanation:
+ * 
+ * There is no string in strs that can be rearranged to form "bat".
+ * The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+ * The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+ * Example 2:
+ * 
+ * Input: strs = [""]
+ * 
+ * Output: [[""]]
+ * 
+ * Example 3:
+ * 
+ * Input: strs = ["a"]
+ * 
+ * Output: [["a"]]
+ * 
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ */
+
+function groupAnagrams(strs: string[]): string[][] {
+    let anagramGroups = new Map<string, string[]>()
+
+    for (let i = 0; i < strs.length; i++) {
+        let sorted = strs[i].split('').sort().join('')
+
+        if (anagramGroups.has(sorted)) {
+            anagramGroups.set(sorted, [...anagramGroups.get(sorted)!, strs[i]])
+        } else {
+            anagramGroups.set(sorted, [strs[i]])
+        }
+    }
+
+    return Array.from(anagramGroups.values())
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
