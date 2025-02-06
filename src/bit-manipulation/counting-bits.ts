921fccd25151a9c4752a93c6d238ef830df1d876
chicio <fabrizio.duroni@gmail.com>
2025-02-06 22:05:05 +0100

    Adding exercises :rocket:

--- src/bit-manipulation/counting-bits.ts
+++ src/bit-manipulation/counting-bits.ts
@@ -42,6 +42,7 @@ function countBits(n: number): number[] {
     let numberOfOneBitsForEachNumber = Array(n + 1).fill(0)
 
     for (let i = 1; i <= n; i++) {
+        //dp[n] = dp[n - 2^k] + 1, where 2^k is the largest power of 2 ≤ n.
         let biggestPowerOfTwoMinorOfI = Math.pow(2, Math.floor(Math.log2(i)))
         let startingSequence = numberOfOneBitsForEachNumber[i - biggestPowerOfTwoMinorOfI]
         numberOfOneBitsForEachNumber[i] = startingSequence + 1
