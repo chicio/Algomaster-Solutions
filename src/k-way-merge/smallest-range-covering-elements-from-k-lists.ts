a839ec39c46547068a4477978b3dbdbfe25ed5ca
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-10-06 20:51:45 +0200

    Adding exercises :rocket:

--- src/k-way-merge/smallest-range-covering-elements-from-k-lists.ts
+++ src/k-way-merge/smallest-range-covering-elements-from-k-lists.ts
@@ -1,14 +1,9 @@
 /**
  * https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/description/
  * 632. Smallest Range Covering Elements from K Lists
- * Solved
- * Hard
- * 
- * Topics
- * premium lock icon
- * Companies
+ *
  * You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
- * 
+ *
  * We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
  * 
  *  
@@ -35,7 +30,6 @@
  * -105 <= nums[i][j] <= 105
  * nums[i] is sorted in non-decreasing order.
  */
-
 
 import { Heap } from "../heap";
 
@@ -78,3 +72,5 @@ function smallestRange(nums: number[][]): number[] {
 
   return bestRange;
 }
+
+console.log(smallestRange([[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]));
