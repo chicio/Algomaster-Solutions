b49496e5a086f1aab180f2e8b2ba4a6e6eda79d0
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-01-07 02:13:36 +0100

    Adding exercises :rocket:

---
+++ src/stacks/basic-calculator-II.ts
@@ -0,0 +1,75 @@
+/**
+ * https://leetcode.com/problems/basic-calculator-ii/description/
+ * 227. Basic Calculator II
+ * 
+ * Given a string s which represents an expression, evaluate this expression and return its value. 
+ * 
+ * The integer division should truncate toward zero.
+ * 
+ * You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+ * 
+ * Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: s = "3+2*2"
+ * Output: 7
+ * Example 2:
+ * 
+ * Input: s = " 3/2 "
+ * Output: 1
+ * Example 3:
+ * 
+ * Input: s = " 3+5 / 2 "
+ * Output: 5
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 3 * 105
+ * s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+ * s represents a valid expression.
+ * All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+ * The answer is guaranteed to fit in a 32-bit integer.
+ */
+
+function calculate(s: string): number {
+  const stack: number[] = [];
+  let num = 0;
+  let sign = '+';
+
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+
+    if (c >= '0' && c <= '9') {
+      num = num * 10 + (c.charCodeAt(0) - 48);
+    }
+
+    if ((c < '0' || c > '9') && c !== ' ' || i === s.length - 1) {
+      if (sign === '+') {
+        stack.push(num);
+      } else if (sign === '-') {
+        stack.push(-num);
+      } else if (sign === '*') {
+        stack.push(stack.pop()! * num);
+      } else if (sign === '/') {
+        const prev = stack.pop()!;
+        stack.push(prev < 0
+          ? Math.ceil(prev / num)
+          : Math.floor(prev / num)
+        );
+      }
+
+      sign = c;
+      num = 0;
+    }
+  }
+
+  return stack.reduce((a, b) => a + b, 0);
+}
+
+console.log(calculate("3+2*2")); // 7
+console.log(calculate(" 3/2 ")); // 1
+console.log(calculate(" 3+5 / 2 ")); // 5
