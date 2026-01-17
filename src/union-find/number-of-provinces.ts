e2ad86be62837be2c92daa8e9d8c96fed388488a
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-01-17 16:48:20 +0100

    Adding exercises :rocket:

--- src/union-find/number-of-provinces.ts
+++ src/union-find/number-of-provinces.ts
@@ -33,46 +33,7 @@
  * isConnected[i][j] == isConnected[j][i]
  */
 
-class UnionFind {
-    parent: number[]
-    rank: number[]
-
-    constructor(n: number) {
-        this.parent = Array(n)
-        this.rank = Array(n).fill(0)
-
-        for (let i = 0; i < n; i++) {
-            this.parent[i] = i
-        }
-    }
-
-    find(x: number): number {
-        if (this.parent[x] !== x) {
-            this.parent[x] = this.find(this.parent[x]) // path compression
-        }
-        return this.parent[x]
-    }
-
-    union(x: number, y: number): boolean {
-        const rootX = this.find(x)
-        const rootY = this.find(y)
-
-        if (rootX === rootY) {
-            return false
-        }
-
-        if (this.rank[rootX] < this.rank[rootY]) {
-            this.parent[rootX] = rootY
-        } else if (this.rank[rootX] > this.rank[rootY]) {
-            this.parent[rootY] = rootX
-        } else {
-            this.parent[rootY] = rootX
-            this.rank[rootX]++
-        }
-
-        return true
-    }
-}
+import { UnionFind } from "../union-find"
 
 function findCircleNum(isConnected: number[][]): number {
     const n = isConnected.length
@@ -92,3 +53,5 @@ function findCircleNum(isConnected: number[][]): number {
 
     return count
 };
+
+console.log(findCircleNum([[1,1,0],[1,1,0],[0,0,1]]))
