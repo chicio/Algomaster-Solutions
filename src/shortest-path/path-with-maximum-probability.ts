0e35031a56394e48a80c74bb6cf4706748ac35bd
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-02-14 18:33:48 +0100

    Adding exercises : rocket:

--- src/shortest-path/path-with-maximum-probability.ts
+++ src/shortest-path/path-with-maximum-probability.ts
@@ -1,4 +1,5 @@
 /**
+ * https://leetcode.com/problems/path-with-maximum-probability/description/
  * 1514. Path with Maximum Probability
  * 
  * You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
