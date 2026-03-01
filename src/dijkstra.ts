755f149d06370d0cd1b84490d20d4dd6e47d38e4
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-03-01 17:04:48 +0100

    Adding exercises :rocket:

--- src/dijkstra.ts
+++ src/dijkstra.ts
@@ -35,3 +35,51 @@ export function dijkstra(
 
     return dist;
 }
+
+type MatrixElement = { row: number; column: number; cost: number };
+
+export function dijkstraMatrix(
+    graph: number[][],
+    distanceInit: number = 0,
+    newCostCondition: (currentCost: number, currentValue: number, nextValue: number) => number
+): number {
+    const directions = [[0, 1], [-1, 0], [0, -1], [1, 0]]
+    const rows = graph.length
+    const columns = graph[0].length
+    const dist: number[][] = Array.from({ length: rows }, () => Array(columns).fill(Infinity));
+    dist[0][0] = distanceInit;
+
+    const minCostHeap = new Heap<MatrixElement>((a, b) => a.cost - b.cost);
+    minCostHeap.insert({ row: 0, column: 0, cost: distanceInit });
+
+    while (minCostHeap.size() > 0) {
+        const current = minCostHeap.extract()!;
+        const { row, column, cost } = current;
+
+        if (cost > dist[row][column]) { 
+            continue;
+        }
+
+        if (row == rows - 1 && column === columns - 1) {
+            return cost
+        }
+
+        for (const [moveX, moveY] of directions) {
+            const newRow = row + moveX
+            const newColumn = column + moveY
+
+            if (newRow < 0 || newRow >= rows || newColumn < 0 || newColumn >= columns) {
+                continue;
+            }
+
+            const newCost = newCostCondition(cost, graph[row][column], graph[newRow][newColumn]);
+
+            if (newCost < dist[newRow][newColumn]) {
+                dist[newRow][newColumn] = newCost;
+                minCostHeap.insert({ row: newRow, column: newColumn, cost: newCost });
+            }
+        }
+    }
+
+    return 0;
+}
--- src/shortest-path/path-with-minimum-effort.ts
+++ src/shortest-path/path-with-minimum-effort.ts
@@ -41,57 +41,10 @@
  * 1 <= heights[i][j] <= 106
  */
 
-import { Heap } from "../heap";
-
-type MatrixElement = { row: number; column: number; cost: number };
-
-function dijkstraMatrix(
-    graph: number[][],
-): number {
-    const directions = [[0, 1], [-1, 0], [0, -1], [1, 0]]
-    const rows = graph.length
-    const columns = graph[0].length
-    const dist: number[][] = Array.from({ length: rows }, () => Array(columns).fill(Infinity));
-    dist[0][0] = 0;
-
-    const minCostHeap = new Heap<MatrixElement>((a, b) => a.cost - b.cost);
-    minCostHeap.insert({ row: 0, column: 0, cost: 0 });
-
-    while (minCostHeap.size() > 0) {
-        const current = minCostHeap.extract()!;
-        const { row, column, cost } = current;
-
-        if (cost > dist[row][column]) { 
-            continue;
-        }
-
-        if (row == rows - 1 && column === columns - 1) {
-            return cost
-        }
-
-        for (const [moveX, moveY] of directions) {
-            const newRow = row + moveX
-            const newColumn = column + moveY
-
-            if (newRow < 0 || newRow >= rows || newColumn < 0 || newColumn >= columns) {
-                continue;
-            }
-
-            const edgeCost = Math.abs(graph[row][column] - graph[newRow][newColumn])
-            const newCost = Math.max(cost, edgeCost);
-
-            if (newCost < dist[newRow][newColumn]) {
-                dist[newRow][newColumn] = newCost;
-                minCostHeap.insert({ row: newRow, column: newColumn, cost: newCost });
-            }
-        }
-    }
-
-    return 0;
-}
+import { dijkstraMatrix } from "../dijkstra";
 
 function minimumEffortPath(heights: number[][]): number {
-    return dijkstraMatrix(heights)
+    return dijkstraMatrix(heights, 0, (currentCost, currentValue, nextValue) => Math.max(currentCost, Math.abs(currentValue - nextValue)));
 };
 
 console.log(minimumEffortPath([[1, 2, 2], [3, 8, 2], [5, 3, 5]])) // 2
